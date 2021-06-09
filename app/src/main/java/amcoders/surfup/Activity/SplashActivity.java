3d06fda6f494d4eb430a00799ae1b0a0d92ab972
Sanjay Thiyagarajan <sanjayipscoc@gmail.com>
2021-06-09 12:14:55 +0530

    Added SplashScreen functionality

---
+++ app/src/main/java/amcoders/surfup/Activity/SplashActivity.java
@@ -0,0 +1,47 @@
+package amcoders.surfup.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+import amcoders.surfup.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private static int SPLASH_SCREEN_TIME_OUT=4000;
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i;
+                if (mAuth.getCurrentUser() == null)
+                {
+                    i=new Intent(SplashActivity.this,
+                            SignUpActivity.class);
+                }
+                else {
+                    i = new Intent(SplashActivity.this,
+                            MainActivity.class);
+                }
+
+                startActivity(i);
+
+                finish();
+            }
+        }, SPLASH_SCREEN_TIME_OUT);
+
+    }
+
+}
